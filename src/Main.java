f6c6af3b7f850a51544e6355995df11d7bbd8e7b
Диана Бережная <Lacshri@yandex.ru>
2022-03-21 01:04:49 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int initialBalance = 200;
+        int amount = 1500;
+        int bonus = amount / 100;
+
+        {
+            if (amount < 1000)
+                bonus = 0;
+        }
+        int total = initialBalance + amount + bonus;
+        System.out.println(total);
+        System.out.println(bonus);
+    }
+}
